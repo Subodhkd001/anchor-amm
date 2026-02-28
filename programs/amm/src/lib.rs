0b0ba1b44a3d1c78e920cc1f2a5775314b37cf5f
Subodh <123922052+Subodhkd001@users.noreply.github.com>
2026-02-28 16:06:04 +0530

    update lib .rs

--- programs/amm/src/lib.rs
+++ programs/amm/src/lib.rs
@@ -1,21 +1,34 @@
-pub mod constants;
-pub mod error;
-pub mod instructions;
-pub mod state;
-
 use anchor_lang::prelude::*;
 
-pub use constants::*;
-pub use instructions::*;
-pub use state::*;
+mod errors;
+mod instructions;
+mod state;
 
-declare_id!("C8Gs3TfuL9szJCgMpYzK73bz3c3BxkwBgWEDLkTKN6Br");
+use instructions::*;
+declare_id!("Abckz5L9LZix4zXYmqJLzKBwRpm9aJ539J26UsZSrLWN");
 
 #[program]
-pub mod amm {
+pub mod anchor_amm_q4_25 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
-    }    
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64,
+        fee: u16,
+        authority: Option<Pubkey>,
+    ) -> Result<()> {
+        ctx.accounts.init(seed, fee, authority, ctx.bumps)
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y)
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount, max_x, max_y)
+    }
+
+    pub fn swap(ctx: Context<Swap>, is_x: bool, amount_in: u64, min_amount_out: u64) -> Result<()> {
+        ctx.accounts.swap(is_x, amount_in, min_amount_out)
+    }
 }
